e1225b049512f88151b98e57d8a56026f84a7dbb
Junichi Shinohara <junichi.shinohara@plaid.co.jp>
2023-11-20 14:13:38 +0900

    a11y

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <div className="header">Header</div>
-      <div className="mainContents">I'm Junichi Shinohara</div>
+      <div className="mainContents">I&apos;m Junichi Shinohara</div>
       <ul className="links">
         <li><Link href="/profile">Go to Profile</Link></li>
         <li><Link href="/portfolio">Go to Portfolio</Link></li>
